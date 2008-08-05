7aac38266021684ac1ca03438af61f2545b3418d
avery.bunker@gmail.com <avery.bunker@gmail.com@fa67d620-b853-0410-97be-15c1e943dd0f>
2008-08-05 20:09:55 +0000

    Login Window Added Renamed Form1 to frmMain

--- trunk/DataPowerFileManager/Program.cs
+++ trunk/DataPowerFileManager/Program.cs
@@ -15,7 +15,7 @@ namespace DataPowerFileManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
