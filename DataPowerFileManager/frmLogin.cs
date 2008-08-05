904540af0dcf358aed1289f74c73b27b55ae7bb7
avery.bunker@gmail.com <avery.bunker@gmail.com@fa67d620-b853-0410-97be-15c1e943dd0f>
2008-08-05 20:08:30 +0000

    Added frmLogin     Username     Password     DataPower IP

---
+++ DataPowerFileManager/frmLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataPowerFileManager
+{
+    public partial class frmLogin : Form
+    {
+        float fltOpacity = 0.01F;
+
+        public frmLogin()
+        {            
+            InitializeComponent();           
+        }
+
+        private void Startup()
+        {
+            tOpacity.Start();            
+        }
+
+        
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            frmMain frm = new frmMain();
+            frm.Visible = true;
+            this.Visible = false;
+        }
+
+        private void tOpacity_Tick(object sender, EventArgs e)
+        {
+            fltOpacity = fltOpacity + .1F;
+            this.Opacity = fltOpacity;
+        }
+    }
+}
