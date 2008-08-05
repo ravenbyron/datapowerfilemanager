6fb2508e6fbd6e1fbfcd14e464dca3f5578a9ac4
avery.bunker@gmail.com <avery.bunker@gmail.com@fa67d620-b853-0410-97be-15c1e943dd0f>
2008-08-05 20:06:09 +0000

    Renamed form1 to frmMain

---
+++ DataPowerFileManager/frmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DataPowerFileManager
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {           
+            if (disposing && (components != null))
+            {
+                
+                components.Dispose();
+                frmLogin frm = new frmLogin();
+                frm.Visible = true;
+                //this.Close();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(137, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get Domains";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Get File List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 106);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(304, 178);
+            this.treeView1.TabIndex = 7;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.BeepOnError = true;
+            this.maskedTextBox1.Location = new System.Drawing.Point(137, 41);
+            this.maskedTextBox1.Mask = "000.000.000.000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(121, 20);
+            this.maskedTextBox1.TabIndex = 8;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 296);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataPowerFileManager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ DataPowerFileManager/frmMain.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DataPowerFileManager.WebReference;
+using DataPowerFileManager.com.prolifics.dpowerxi50;
+
+namespace DataPowerFileManager
+{
+    public partial class frmMain : Form
+    {
+
+        appmgmtprotocol foo = new appmgmtprotocol();
+        xmlmgmt bar = new xmlmgmt();
+        String[] combobox = new string[100];
+        String[] tmp = new string[1];
+        String[] list = new string[100];
+        String domain;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            combobox[0] = "default";
+            comboBox1.DataSource = combobox;
+            comboBox1.SelectedIndex = 0;
+          
+        }
+
+        private void GetDataPowerDomains()
+        {
+            //String[] fooarray = new string[100];
+            GetDomainListRequest gdr = new GetDomainListRequest();
+            foo.GetDomainList(gdr).Items.CopyTo(combobox, 1);
+            comboBox1.DataSource = tmp;
+            comboBox1.DataSource = combobox;
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetDataPowerDomains();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            domain = combobox[comboBox1.SelectedIndex];
+            textBox1.Text = domain;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            requestGetfilestore rgf = new requestGetfilestore();
+            rgf.location = filestorelocation.local;
+            rgf.annotated = true;
+            rgf.layoutonly = false;
+            request xmlr = new request();
+            xmlr.domain = domain;
+            xmlr.Item = rgf;
+            response xmlres;
+            xmlres = bar.operation(xmlr);
+            responseFilestore xmlres1;
+            xmlres1 = (responseFilestore) xmlres.Item;
+
+
+
+            textBox1.Text = xmlres1.ToString();
+            textBox1.Text = xmlres1.Any[0].InnerXml.ToString();
+            //treeView1.da
+             
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        
+
+        
+    }
+}
