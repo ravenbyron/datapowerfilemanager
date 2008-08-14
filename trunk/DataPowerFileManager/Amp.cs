c7847f14e3e71722787ecb86409289a2cb6e2276
avery.bunker@gmail.com <avery.bunker@gmail.com@fa67d620-b853-0410-97be-15c1e943dd0f>
2008-08-14 15:51:50 +0000

    Added New Class for the AMP Initial Call

---
+++ trunk/DataPowerFileManager/Amp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataPowerFileManager.WebReference;
+using DataPowerFileManager.com.prolifics.dpowerxi50;
+
+namespace DataPowerFileManager
+{
+    class Amp
+    {
+        private static appmgmtprotocol _amp = new appmgmtprotocol();
+
+        static Amp()
+        {
+        }
+
+        public static appmgmtprotocol GetAppInstance()
+        {
+            return _amp;
+        }
+    }
+}
