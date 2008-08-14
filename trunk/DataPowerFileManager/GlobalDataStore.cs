b612e3b28f6b1777820c8e7792c4773bdc54adc8
avery.bunker@gmail.com <avery.bunker@gmail.com@fa67d620-b853-0410-97be-15c1e943dd0f>
2008-08-14 03:11:10 +0000

    Added a Global Transport Layer

---
+++ trunk/DataPowerFileManager/GlobalDataStore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataPowerFileManager
+{
+    class GlobalDataStore
+    {
+        private static GlobalDataStore _instance = new GlobalDataStore();
+        private string _strDataPowerHost = "";
+        private string _strDataPowerPort = "";
+
+        static GlobalDataStore()
+        {
+        }
+
+        public static GlobalDataStore GetInstance()
+        {
+            return _instance;
+        }
+
+        public string strDataPowerHost
+        {
+            get
+            {
+                return _strDataPowerHost;
+            }
+            set
+            {
+                _strDataPowerHost = value;
+            }
+        }
+
+        public string strDataPowerPort
+        {
+            get
+            {
+                return _strDataPowerPort;
+            }
+            set
+            {
+                _strDataPowerPort = value;
+            }
+        }
+    }
+}
